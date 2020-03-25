181b4c2f4ceca43ba602c15f27f485e33ba46cd4
anzamarska <anzamarska@gmail.com>
2020-03-25 15:21:03 +0100

    day 9 task done

--- 09 - Dev Tools Domination/script.js
+++ 09 - Dev Tools Domination/script.js
@@ -7,25 +7,68 @@ function makeGreen() {
 }
 
 // Regular
+console.log('Hello');
 
 // Interpolated
+console.log('Hello, my name is %s Zam', 'Ann');
 
 // Styled
+console.log('%cHello', 'font-size:20px; background:green');
+////// %c at the begining of first string
 
 // warning!
+console.warn('OH NOOOOO!');
 
 // Error :|
+console.error('Shit!');
 
 // Info
+console.info('info part');
 
 // Testing
+console.assert(1 === 2, 'That is wrong');
+///if first part is true nothing happens, if not print in console that second part
 
 // clearing
+console.clear();
+
 
 // Viewing DOM Elements
 
+console.dir();
+///all the different properties
+
 // Grouping together
+dogs.forEach(dog => {
+  console.group(`${dog.name}`);
+  // group could be changed to groupCollapsed
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+})
+
 
 // counting
 
+console.count('Wes');
+console.count('Wes');
+console.count('Wes');
+console.count('Wes');
+console.count('Ann');
+console.count('Ann');
+console.count('Wes');
+console.count('Wes');
+
+
 // timing
+
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data');
+    console.log(data);
+  });
+
+console.table(dogs);
