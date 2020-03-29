88f85a06dca35da666d882ce972edfbf969a04e2
anzamarska <anzamarska@gmail.com>
2020-03-29 15:41:38 +0200

    add some more done task

--- 14 - JavaScript References VS Copying/script.js
+++ 14 - JavaScript References VS Copying/script.js
@@ -2,6 +2,7 @@
 
     // Let's say we have an array
     const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+    console.log(players);
 
     // and we want to make a copy of it.
 
@@ -17,11 +18,28 @@
 
     // So, how do we fix this? We take a copy instead!
 
+    const team = players.slice();
+    team[1] = 'ANIANIANAI';
+    console.log(players);
+    console.log("after slice", team);
+
     // one way
 
     // or create a new array and concat the old one in
 
+    const team3 = [].concat(players);
+    team3[3] = 'BLA BLA ';
+    console.log("after concat and bla bla", team3);
+    console.log(players);
+
     // or use the new ES6 Spread
+
+    const team4 = [...players];
+    team4[3] = 'heeee hawww';
+    console.log(team4);
+
+    const team5 = Array.from(players);
+    console.log(team5);
 
     // now when we update it, the original one isn't changed
 
@@ -36,7 +54,37 @@
     // and think we make a copy:
 
     // how do we take a copy instead?
+    const cap2 = Object.assign({}, person, { number: 99, age: 12 });
+    console.log("cap2", cap2);
+    console.log("person", person);
 
     // We will hopefully soon see the object ...spread
 
+    const cap3 = {...person};
+    cap3.age = 345;
+    console.log("cap3", cap3);
+    console.log("person", person);
+
     // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+
+
+    const wes = {
+      name: 'Wes',
+      age: 100,
+      social: {
+        twitter: '@wesbos',
+        facebook: 'wesbos.developer'
+      }
+    };
+
+        console.log(wes);
+
+    const dev = Object.assign({}, wes);
+    dev.social.facebook = "devfb";
+    console.log('dev', dev);
+    console.log('wes', wes);
+
+    const dev2 = JSON.parse(JSON.stringify(wes));
+    dev2.social.facebook = "dev2fb";
+    console.log('dev2', dev2);
+    console.log('wes',wes);
