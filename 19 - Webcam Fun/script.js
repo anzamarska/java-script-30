30e63d875321aa2916cc8e24ab497a76791fa49a
anzamarska <anzamarska@gmail.com>
2020-07-29 16:47:47 +0200

    add function changing colour of video

--- 19 - Webcam Fun/script.js
+++ 19 - Webcam Fun/script.js
@@ -23,7 +23,39 @@ function paintToCanvas() {
   console.log(width, height);
   canvas.width = width;
   canvas.height = height;
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+    // console.log(pixels);
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
 }
 
-console.log("media :");
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "picture that i made");
+  link.innerHTML = `<img src="${data}" alt="photo" />`;
+  // link.textContent = "Download Image";
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200;
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // G
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // B
+  }
+  return pixels;
+}
+
+// paintToCanvas();
+// console.log("media :");
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
